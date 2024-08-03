ce3a04a9b44bd86233198119eea3b64915a36052
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2024-08-03 16:03:16 +0530

    Time: 2 ms (52.94%), Space: 41.7 MB (21.13%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for(int i=0;i<s.length(); i++){
+            char bracket = s.charAt(i);
+            if(bracket== '('){
+                stack.push(')');
+            } else if(bracket == '{'){
+                stack.push('}');
+            } else if(bracket == '['){
+                stack.push(']');
+            } else if(stack.isEmpty() || stack.pop() !=bracket){
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
