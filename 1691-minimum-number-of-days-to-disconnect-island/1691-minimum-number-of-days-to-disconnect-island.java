1504924eb77b3a72fbaa006066a7c8aba7736a93
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2024-08-11 16:00:28 +0530

    Time: 30 ms (56.82%), Space: 44 MB (56.06%) - LeetHub

---
+++ 1691-minimum-number-of-days-to-disconnect-island/1691-minimum-number-of-days-to-disconnect-island.java
@@ -0,0 +1,47 @@
+ class Solution {
+    private void dfs(int[][] grid, boolean[][] visited, int i, int j) {
+        int m = grid.length, n = grid[0].length;
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0 || visited[i][j]) 
+            return;
+        visited[i][j] = true;
+        dfs(grid, visited, i + 1, j);
+        dfs(grid, visited, i - 1, j);
+        dfs(grid, visited, i, j + 1);
+        dfs(grid, visited, i, j - 1);
+    }
+
+    private int countIslands(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    count++;
+                    dfs(grid, visited, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    
+    public int minDays(int[][] grid) {
+        if (countIslands(grid) != 1) 
+            return 0;
+        
+        int m = grid.length, n = grid[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    grid[i][j] = 0;
+                    if (countIslands(grid) != 1) 
+                        return 1;
+                    grid[i][j] = 1;
+                }
+            }
+        }
+        return 2;
+    }
+}
