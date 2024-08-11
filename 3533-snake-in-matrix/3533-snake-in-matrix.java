c7ca44c6c5034bb46900981c9a2b692320a40b91
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2024-08-12 00:50:33 +0530

    Time: 2 ms (100%), Space: 44.5 MB (100%) - LeetHub

---
+++ 3533-snake-in-matrix/3533-snake-in-matrix.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int finalPositionOfSnake(int n, List<String> c) {
+        int[][] v = new int[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                v[i][j] = (i * n) + j;
+            }
+        }
+        
+        int x = 0, y = 0;
+        int k = c.size();
+        
+        for (int i = 0; i < k; i++) {
+            String direction = c.get(i);
+            if (direction.equals("UP")) {
+                x -= 1;
+            } else if (direction.equals("RIGHT")) {
+                y += 1;
+            } else if (direction.equals("DOWN")) {
+                x += 1;
+            } else if (direction.equals("LEFT")) {
+                y -= 1;
+            }
+        }
+        
+        return v[x][y];
+    }
+}
