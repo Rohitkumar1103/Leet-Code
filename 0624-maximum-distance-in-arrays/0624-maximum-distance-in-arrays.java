d3a714d1401f4a9db95684e0fd35a301503cb78d
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2024-08-16 22:04:55 +0530

    Time: 49 ms (5.06%), Space: 64.7 MB (37.97%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
@@ -1,25 +1,17 @@
 class Solution {
     public int maxDistance(List<List<Integer>> arrays) {
-       int siz = arrays.size();
-        int min_0 = arrays.get(0).get(0);
-        int max_0 = arrays.get(0).get(arrays.get(0).size() - 1);
+        Collections.sort(arrays,(l1,l2) -> {
+            return l1.get(0) - l2.get(0);
+        });
         int maxDistance = 0;
-
-        // Iterate through the remaining arrays
-        for (int i = 1; i < siz; i++) {
-            List<Integer> arr = arrays.get(i);
-            int currentMin = arr.get(0);
-            int currentMax = arr.get(arr.size() - 1);
-
-            // Calculate the possible max distances
-            maxDistance = Math.max(maxDistance, Math.abs(max_0 - currentMin));
-            maxDistance = Math.max(maxDistance, Math.abs(currentMax - min_0));
-
-            // Update the min_0 and max_0 for future comparisons
-            min_0 = Math.min(min_0, currentMin);
-            max_0 = Math.max(max_0, currentMax);
+        int start = arrays.get(0).get(0);
+        int end = arrays.get(0).getLast();
+        for(int i = 1; i < arrays.size(); i ++) {
+            List<Integer> list = arrays.get(i);
+            int dist1 = list.get(list.size() - 1) - start;
+            int dist2 = Math.abs(end - list.get(0));
+            maxDistance = Math.max(maxDistance,Math.max(dist1,dist2));
         }
-
         return maxDistance;
     }
 }
