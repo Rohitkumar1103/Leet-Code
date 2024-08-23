2ec20b1f1f1ea3fde13527dc7de97e11865d2dd7
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2024-08-24 04:36:58 +0530

    Time: 1 ms (100%), Space: 42.2 MB (22.13%) - LeetHub

--- 3426-minimum-number-of-chairs-in-a-waiting-room/3426-minimum-number-of-chairs-in-a-waiting-room.java
+++ 3426-minimum-number-of-chairs-in-a-waiting-room/3426-minimum-number-of-chairs-in-a-waiting-room.java
@@ -1,12 +1,13 @@
 class Solution {
     public int minimumChairs(String s) {
-        int count = 0, minChair = 0; // count
+        int count = 0, minChair = 0; //count tracks the current number of people in the room. |||  minChair track the maximum number of people in the room at any point, which corresponds to the minimum number of chairs needed.
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i) == 'E'){
                 count++;
             } else{
                 count--;
             }
+            //After each step, update ans to be the maximum value between count and ans. This ensures that ans always reflects the maximum number of people that have been in the room at any given point.
             minChair = Math.max(count, minChair);
         }
         return minChair;
