e900b48c44dbb861dfb95dc05f64fb26ecf0c5db
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2024-08-26 20:56:23 +0530

    Time: 0 ms (100%), Space: 41.8 MB (67.55%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,13 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int matrix[][] = new int[nums.length][2];
-        for(int i=0; i< nums.length; i++){
-            matrix[i][0] = i;
-            matrix[i][1] = nums[i];
-        }
-        for(int i=0; i< matrix.length; i++){
-            if(matrix[i][1] == target){
-                return matrix[i][0];
+        // int matrix[][] = new int[nums.length][2];
+        // for(int i=0; i< nums.length; i++){
+        //     matrix[i][0] = i;
+        //     matrix[i][1] = nums[i];
+        // }
+        // for(int i=0; i< matrix.length; i++){
+        //     if(matrix[i][1] == target){
+        //         return matrix[i][0];
+        //     }
+        // }
+        // return -1;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == target){
+                return i;
             }
         }
         return -1;
