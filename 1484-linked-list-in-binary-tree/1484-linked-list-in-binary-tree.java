c5f6ee2697847f85c87652de7fd31aa82c443fa1
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2024-09-07 19:29:45 +0530

    Time: 0 ms (100%), Space: 45.5 MB (8.29%) - LeetHub

---
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.java
@@ -0,0 +1,21 @@
+class Solution {
+    private boolean path(ListNode head, TreeNode root,ListNode th){
+        if(head==null) return true;
+        if(root==null) return false;
+
+        if(root.val==head.val)
+          return path(head.next, root.left,th)||
+            path(head.next,root.right,th);
+
+        if(th.val==root.val) 
+        return path(head,root.left,th.next) || path(head,root.right,th.next);
+
+        else
+        return path(th,root.left,th) || path(th,root.right,th);
+    }
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if(head==null) return true;
+        if(root==null) return false;
+        return path(head,root,head);
+    }
+}
