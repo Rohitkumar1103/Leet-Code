16412917ac32bebb54d29afe64ee5b2c626c9c2b
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2024-10-06 19:26:32 +0530

    Time: 1 ms (96.43%), Space: 42 MB (13.89%) - LeetHub

--- 1923-sentence-similarity-iii/1923-sentence-similarity-iii.java
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.java
@@ -20,17 +20,12 @@ class Solution {
         int start = 0, end = 0;
         int n1 = words1.length, n2 = words2.length;
 
-        // Compare from the start
         while (start < n2 && words1[start].equals(words2[start])) {
             start++;
         }
-
-        // Compare from the end
         while (end < n2 && words1[n1 - end - 1].equals(words2[n2 - end - 1])) {
             end++;
         }
-
-        // Check if the remaining unmatched part is in the middle
         return start + end >= n2;
     }
 }
