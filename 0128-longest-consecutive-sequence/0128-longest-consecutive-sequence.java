901d9627b9076437827974317eee37d982b22cb5
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2025-08-23 21:17:48 +0530

    Time: 17 ms (94.21%), Space: 56.9 MB (80.48%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,21 +1,42 @@
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int num :  nums){
+//             set.add(num);
+//         }
+//         int maxLength = 0;
+//         for(int num : set){
+//             if(!set.contains(num-1)){
+//                 int currentNum = num;
+//                 int length = 1;
+//                 while (set.contains(currentNum + 1)) {
+//                     currentNum++;
+//                     length++;
+//                 }
+//                 maxLength = Math.max(maxLength, length);
+//             }
+//         }
+//     return maxLength;
+//     }
+// }
+
+
+
 class Solution {
     public int longestConsecutive(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int num :  nums){
-            set.add(num);
-        }
-        int maxLength = 0;
-        for(int num : set){
-            if(!set.contains(num-1)){
-                int currentNum = num;
-                int length = 1;
-                while (set.contains(currentNum + 1)) {
-                    currentNum++;
-                    length++;
-                }
-                maxLength = Math.max(maxLength, length);
+        if (nums.length == 0) return 0; // handle empty array
+
+        Arrays.sort(nums);
+        int count = 1, maxLen = 1;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i+1] == nums[i] + 1) {
+                count++;
+                maxLen = Math.max(maxLen, count);
+            } else if (nums[i+1] != nums[i]) {
+                count = 1; // reset if not consecutive (ignore duplicates)
             }
         }
-    return maxLength;
+        return maxLen;
     }
 }
