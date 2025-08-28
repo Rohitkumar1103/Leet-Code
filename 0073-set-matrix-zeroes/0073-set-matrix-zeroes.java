404e29cefa057133776fd72ac8aeb5798c3d7441
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2025-08-29 04:00:39 +0530

    Time: 0 ms (100%), Space: 46.2 MB (8.43%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,23 +1,80 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int marker = (int) -1e9;
+        int m = matrix.length, n = matrix[0].length;
+        boolean firstRowZero = false, firstColZero = false;    
 
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(matrix[i][j] == 0){
-                    for(int col = 0; col<matrix[0].length; col++){
-                        if(matrix[i][col] != 0) matrix[i][col] = marker;
-                    }
-                    for(int row = 0; row<matrix.length; row++){
-                        if(matrix[row][j] != 0) matrix[row][j] = marker;
-                    }
+        //CHECK IF FIRST ROW HAS ANY ZERO
+        for(int j=0; j<n; j++){
+            if(matrix[0][j] == 0){
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        for(int i=0; i<m; i++){
+            if(matrix[i][0] == 0){
+                firstColZero = true;
+                break;
+            }
+        }    
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(matrix[i][j] == marker) matrix[i][j] = 0;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
 }
+
+
+
+
+
+
+//BRUTE FORCE APPROACH
+// class Solution {
+//     public void setZeroes(int[][] matrix) {
+//         int marker = (int) -1e9;
+
+//         for(int i=0; i<matrix.length; i++){
+//             for(int j=0; j<matrix[0].length; j++){
+//                 if(matrix[i][j] == 0){
+//                     for(int col = 0; col<matrix[0].length; col++){
+//                         if(matrix[i][col] != 0) matrix[i][col] = marker;
+//                     }
+//                     for(int row = 0; row<matrix.length; row++){
+//                         if(matrix[row][j] != 0) matrix[row][j] = marker;
+//                     }
+//                 }
+//             }
+//         }
+//         for(int i=0; i<matrix.length; i++){
+//             for(int j=0; j<matrix[0].length; j++){
+//                 if(matrix[i][j] == marker) matrix[i][j] = 0;
+//             }
+//         }
+//     }
+// }
