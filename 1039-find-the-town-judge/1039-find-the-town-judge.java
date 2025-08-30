60deefc8c4fbf44ab02ba934e7524d326f356cfe
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2025-08-30 18:55:51 +0530

    Time: 2 ms (99.07%), Space: 54 MB (46.99%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.java
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -1,18 +1,15 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int[] inDegree = new int[n+1];
-        int[] outDegree = new int[n+1];
+        if(n == 1) return 1;
+        int[] count = new int[n+1];
 
-        for(int[] t : trust){
-            int a = t[0], b = t[1];
-            outDegree[a]++;
-            inDegree[b]++;
+        for(int[] edge: trust){
+            count[edge[1]]++;
+            count[edge[0]]--;
         }
-        for(int i = 1; i <= n; i++) {
-            if(inDegree[i] == n-1 && outDegree[i] == 0) {
-                return i;
-            }
-        }
+        for(int i=0; i<n+1; i++){
+            if(count[i] == n-1) return i;
+        } 
         return -1;
     }
 }
