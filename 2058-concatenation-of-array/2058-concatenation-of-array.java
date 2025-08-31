1d96c16e477bf9900211ad3c2f29ff12fd9a4134
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2025-09-01 02:27:15 +0530

    Time: 0 ms (100%), Space: 45.7 MB (17.26%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.java
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -1,13 +1,28 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int[] doubled = new int[2*nums.length];
+        int n = nums.length;
+        int[] result = new int[2 * n];
 
-        for(int i=0; i<nums.length; i++){
-            doubled[i] = nums[i];
-        }
-        for(int i=0; i<nums.length; i++){
-            doubled[nums.length+i] = nums[i];
-        }
-        return doubled;
+        System.arraycopy(nums, 0, result, 0, n);
+        System.arraycopy(nums, 0, result, n, n);
+
+        return result;
     }
 }
+
+
+
+
+// class Solution {
+//     public int[] getConcatenation(int[] nums) {
+//         int[] doubled = new int[2*nums.length];
+
+//         for(int i=0; i<nums.length; i++){
+//             doubled[i] = nums[i];
+//         }
+//         for(int i=0; i<nums.length; i++){
+//             doubled[nums.length+i] = nums[i];
+//         }
+//         return doubled;
+//     }
+// }
