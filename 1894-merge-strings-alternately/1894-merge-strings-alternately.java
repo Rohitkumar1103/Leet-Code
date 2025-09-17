1f1fe2fc0b8083c2e5cf81e441bf19585319426d
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2025-09-17 21:26:07 +0530

    Time: 0 ms (100%), Space: 41.6 MB (94.32%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,16 +1,39 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        StringBuilder sb = new StringBuilder();
-        int i = 0, j = 0;
+        int n = word1.length();
+        int m = word2.length();
+        char[] merged = new char[n+m];  
+        int i = 0 ,j = 0,k = 0;
 
-        while(i < word1.length() && j < word2.length()){
-            sb.append(word1.charAt(i));
-            sb.append(word2.charAt(j));
-            i++; j++;
+        while(i<n && j<m){
+            merged[k++] = word1.charAt(i++);
+            merged[k++] = word2.charAt(j++);
         }
-        if(i<word1.length()) sb.append(word1.substring(i));
-        if(j<word2.length()) sb.append(word2.substring(j));
 
-        return sb.toString();
+        while(i<n){
+            merged[k++] = word1.charAt(i++);
+        }
+        while(j<m){
+            merged[k++] = word2.charAt(j++);
+        }
+        return new String(merged);
     }
 }
+
+
+// class Solution {
+//     public String mergeAlternately(String word1, String word2) {
+//         StringBuilder sb = new StringBuilder();
+//         int i = 0, j = 0;
+
+//         while(i < word1.length() && j < word2.length()){
+//             sb.append(word1.charAt(i));
+//             sb.append(word2.charAt(j));
+//             i++; j++;
+//         }
+//         if(i<word1.length()) sb.append(word1.substring(i));
+//         if(j<word2.length()) sb.append(word2.substring(j));
+
+//         return sb.toString();
+//     }
+// }
