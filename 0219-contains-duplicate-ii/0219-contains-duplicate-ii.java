e08f5dc3363c505c01254ab98856f5603d9c9803
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2025-09-29 06:02:15 +0530

    Time: 18 ms (96.26%), Space: 59.5 MB (82.67%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,15 +1,32 @@
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for(int i=0; i<nums.length; i++){
+//             if(map.containsKey(nums[i])){
+//                 int value = map.get(nums[i]);
+//                 if(i-value <= k){
+//                     return true;
+//                 }
+//             }
+//             map.put(nums[i], i);
+//         }
+//         return false;
+//     }
+// }
+
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for(int i=0; i<nums.length; i++){
-            if(map.containsKey(nums[i])){
-                int value = map.get(nums[i]);
-                if(i-value <= k){
-                    return true;
-                }
+        HashSet<Integer> window = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (!window.add(nums[i])) {
+                return true; // duplicate found in window
             }
-            map.put(nums[i], i);
+            if (window.size() > k) {
+                window.remove(nums[i - k]); // keep window size <= k
+            }
         }
         return false;
     }
