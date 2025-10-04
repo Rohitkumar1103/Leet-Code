e1a382fc09cad376338a4377c16edf199b23fc93
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2025-10-05 00:12:50 +0530

    Time: 42 ms (64.32%), Space: 57.9 MB (39.95%) - LeetHub

---
+++ 2600-maximum-tastiness-of-candy-basket/2600-maximum-tastiness-of-candy-basket.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maximumTastiness(int[] price, int k) {
+        Arrays.sort(price);
+        int left = 0;
+        int right = price[price.length-1] - price[0];
+        int ans = 0;
+        
+        while(left <= right){
+            int mid = left+(right-left)/2;
+            if(canPick(price, k, mid)){
+                ans = mid;
+                left = mid+1;
+            } else{
+                right = mid-1;
+            }
+        }
+        return ans;
+    }
+    private boolean canPick(int[] price, int k, int minDiff){
+        int count = 1;
+        int last = price[0];
+
+        for(int i=0; i<price.length; i++){
+            if(price[i] - last >= minDiff){
+                count++;
+                last = price[i];
+            }
+            if (count >= k) return true;
+        }
+        return false;
+    }
+}
