3e6227fc6ad286281afb12fcfa687bddf39ac0ce
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2025-10-08 16:09:04 +0530

    Time: 2 ms (99.74%), Space: 50.6 MB (16.91%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,17 +1,57 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int greaterThan = nums.length/3;
-        ArrayList<Integer> list = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
 
-        for(int num: nums){
-            map.put(num, map.getOrDefault(num, 0)+1);
-        }
-        for(int num : map.keySet()){
-            if(map.get(num) > greaterThan){
-                list.add(num);
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+
+        for(int num : nums){
+            if(num == candidate1){
+                count1++;
+            } else if(num == candidate2){
+                count2++;
+            } else if(count1 == 0){
+                candidate1 = num;
+                count1 = 1;
+            } else if(count2 == 0){
+                candidate2 = num;
+                count2 = 1;
+            } else{
+                count1--;
+                count2--;
             }
         }
-        return list;
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        if (count1 > n / 3) result.add(candidate1);
+        if (count2 > n / 3) result.add(candidate2);
+
+        return result;
     }
 }
+
+
+
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//         int greaterThan = nums.length/3;
+//         ArrayList<Integer> list = new ArrayList<>();
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for(int num: nums){
+//             map.put(num, map.getOrDefault(num, 0)+1);
+//         }
+//         for(int num : map.keySet()){
+//             if(map.get(num) > greaterThan){
+//                 list.add(num);
+//             }
+//         }
+//         return list;
+//     }
+// }
