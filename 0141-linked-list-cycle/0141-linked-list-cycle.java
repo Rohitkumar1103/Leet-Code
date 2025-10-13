706df23d190c183275451b66f88b368ecaee2004
Rohit Kumar <149893953+Rohitkumar1103@users.noreply.github.com>
2025-10-13 21:41:14 +0530

    Time: 0 ms (100%), Space: 44.4 MB (78.5%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,40 +9,18 @@
  *     }
  * }
  */
-
- //HASHSET
-
-// public class Solution {
-//     public boolean hasCycle(ListNode head) {
-//         HashSet<ListNode> visited = new HashSet<>();
-//         ListNode temp = head;
-
-//         while(temp != null){
-//             if(visited.contains(temp)){
-//                 return true;
-//             }
-//             visited.add(temp);
-//             temp = temp.next;
-//         }
-//         return false;
-//     }
-// }
-
-
-//USING TORTOISE & HARE
-
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null) return false;
-
         ListNode slow = head;
         ListNode fast = head;
 
         while(fast != null && fast.next != null){
-            slow = slow.next;
             fast = fast.next.next;
+            slow = slow.next;
 
-            if(slow == fast) return true;
+            if(fast == slow){
+                return true;
+            }
         }
         return false;
     }
